3666569cb95d366f3a07dc86555d3f46320e0865
Jason Morse <jasonisop@gmail.com>
2013-08-29 09:38:50 -0400

    adding ATL

---
+++ main.lua
@@ -0,0 +1,45 @@
+--[[
+
+
+
+
+--]]
+
+require('scripts/player')
+
+
+
+
+
+function love.load()
+
+end
+
+function love.update(dt)
+	
+end		
+
+function love.mousepressed(x, y, button)
+	
+end
+
+function love.mousereleased(x, y, button)
+	
+end
+
+function love.keypressed(k)
+	
+end
+
+function love.keyreleased(k)
+
+end
+
+function love.draw()
+	
+end
+
+
+function math.clamp(x, min, max)
+	return x < min and min or (x > max and max or x)
+end
